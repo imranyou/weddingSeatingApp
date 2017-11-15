83ba19cfdcbf02944defd4e8191052e77e78cab5
Imran Yousuf <iyousuf@ebay.com>
2017-11-15 14:07:42 -0800

    reacttoolbox

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './assets/react-toolbox/theme.css';
+import theme from './assets/react-toolbox/theme.js';
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+import Button from 'react-toolbox/lib/button/Button';
 
 class App extends Component {
   render() {
     return (
+  <ThemeProvider theme={theme}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <Button label="Test Button" raised primary />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+  </ThemeProvider>
     );
   }
 }
