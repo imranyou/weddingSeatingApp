99f525c6f12b39812fa314c60c8c4be2fd6bb2a2
Imran Yousuf <iyousuf@ebay.com>
2017-11-16 14:25:07 -0800

    DatePicker works

--- src/App.js
+++ src/App.js
@@ -4,21 +4,16 @@ import './App.css';
 import './assets/react-toolbox/theme.css';
 import theme from './assets/react-toolbox/theme.js';
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
-import Button from 'react-toolbox/lib/button/Button';
+import DatePickerTest from './DatePickerTest';
+
+
 
 class App extends Component {
   render() {
     return (
   <ThemeProvider theme={theme}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Button label="Test Button" raised primary />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <DatePickerTest />
       </div>
   </ThemeProvider>
     );
